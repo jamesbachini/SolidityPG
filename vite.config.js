8907fcddcfe6dbdd29f4c3a7be8b56dfe57ff7df
Anon <anon@tmp.com>
2025-09-08 16:15:09 +0100

    push to docs/

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   base: process.env.NODE_ENV === 'production' ? '/SolidityPG/' : '/',
   build: {
-    outDir: 'dist',
+    outDir: 'docs',
   },
   define: {
     global: 'globalThis',
